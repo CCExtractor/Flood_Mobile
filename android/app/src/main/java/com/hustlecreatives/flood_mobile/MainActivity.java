8979896a43ed02d28c3735dae2d15de5779cb83d
Aman Verma <verma1090aman@gmail.com>
2022-04-07 01:27:51 +0530

    fix: introduce fixes for latest version for flutter

--- android/app/src/main/java/com/hustlecreatives/flood_mobile/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.hustlecreatives.flood_mobile;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
---
+++ android/app/src/main/kotlin/com/hustlecreatives/flood_mobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.hustlecreatives.flood_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
