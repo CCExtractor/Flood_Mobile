556f1e5d051ec397c41a4f527ab4dfe96e703e80
Akshatji800 <akshatji.tripathi.com>
2022-08-09 04:05:30 +0530

    Adds date_converter and file_size_helper unit tests

---
+++ test/file_size_helper_unit_test.dart
@@ -0,0 +1,13 @@
+import 'package:flood_mobile/Services/file_size_helper.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+
+  test(
+    "Test File Size Calculating Function",
+        () {
+      final size = filesize(1156498920);
+      expect(size, '1.08 GB');
+    },
+  );
+}
