07d2f229bf89f0cf8db34a3f5beab317aeeaa56c
Akshatji800 <akshatji.tripathi.com>
2022-08-09 04:13:23 +0530

    Resolves formatting issues

--- test/date_converter_unit_test.dart
+++ test/date_converter_unit_test.dart
@@ -2,10 +2,9 @@ import 'package:flood_mobile/Services/date_converter.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-
   test(
     "Test Date Converting Function",
-        () {
+    () {
       final date = dateConverter(timestamp: 1659201146);
       expect(date, '30 / 7 / 2022');
     },
--- test/file_size_helper_unit_test.dart
+++ test/file_size_helper_unit_test.dart
@@ -2,10 +2,9 @@ import 'package:flood_mobile/Services/file_size_helper.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-
   test(
     "Test File Size Calculating Function",
-        () {
+    () {
       final size = filesize(1156498920);
       expect(size, '1.08 GB');
     },
