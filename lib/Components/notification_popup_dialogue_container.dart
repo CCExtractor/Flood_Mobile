896ddfaac8852a3d33bb62f0999149d9f5813afb
amit patel <amitamrutiya2210@gmail.com>
2023-01-30 21:41:12 +0530

    fix new download torrent list on notification dialogue

--- lib/Components/notification_popup_dialogue_container.dart
+++ lib/Components/notification_popup_dialogue_container.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 Widget notificationPopupDialogueContainer({required BuildContext context}) {
+  NotificationApi.getNotifications(context: context);
   return (Provider.of<HomeProvider>(context)
               .notificationModel
               .notifications
