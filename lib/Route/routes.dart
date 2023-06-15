40e764eade9c1f639c657a5d7c4fae6845039af0
amit patel <amitamrutiya2210@gmail.com>
2023-06-15 10:11:13 +0530

    implement additional routes and format files accordingly

--- lib/Route/routes.dart
+++ lib/Route/routes.dart
@@ -4,4 +4,7 @@ class Routes {
   static const String homeScreenRoute = '/homeScreen';
   static const String torrentContentScreenRoute = '/torrentContentScreen';
   static const String streamVideoScreenRoute = '/screamVideoScreen';
+  static const String settingsScreenRoute = '/settingsScreen';
+  static const String onboardingScreenRoute = '/onboardingScreen';
+  static const String torrnetScreenRoute = '/torrentScreen';
 }
