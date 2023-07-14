a52eff9fd62035557aad5ff74a2942e67c690d13
amit patel <amitamrutiya2210@gmail.com>
2023-07-14 21:38:36 +0530

    Implement the WiFi Only and Shutdown When Complete features.

--- lib/Blocs/bloc_provider_list.dart
+++ lib/Blocs/bloc_provider_list.dart
@@ -1,18 +1,19 @@
-import 'package:flood_mobile/Blocs/language_bloc/language_bloc.dart';
-import 'package:flood_mobile/Blocs/user_interface_bloc/user_interface_bloc.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flood_mobile/Blocs/api_bloc/api_bloc.dart';
+import 'package:flood_mobile/Blocs/client_settings_bloc/client_settings_bloc.dart';
 import 'package:flood_mobile/Blocs/filter_torrent_bloc/filter_torrent_bloc.dart';
+import 'package:flood_mobile/Blocs/graph_bloc/graph_bloc.dart';
 import 'package:flood_mobile/Blocs/home_screen_bloc/home_screen_bloc.dart';
+import 'package:flood_mobile/Blocs/language_bloc/language_bloc.dart';
 import 'package:flood_mobile/Blocs/login_screen_bloc/login_screen_bloc.dart';
 import 'package:flood_mobile/Blocs/multiple_select_torrent_bloc/multiple_select_torrent_bloc.dart';
 import 'package:flood_mobile/Blocs/onboarding_main_page_bloc/on_boarding_page_color_bloc.dart';
+import 'package:flood_mobile/Blocs/power_management_bloc/power_management_bloc.dart';
 import 'package:flood_mobile/Blocs/sse_bloc/sse_bloc.dart';
 import 'package:flood_mobile/Blocs/theme_bloc/theme_bloc.dart';
 import 'package:flood_mobile/Blocs/torrent_content_screen_bloc/torrent_content_screen_bloc.dart';
 import 'package:flood_mobile/Blocs/user_detail_bloc/user_detail_bloc.dart';
-import 'package:flood_mobile/Blocs/client_settings_bloc/client_settings_bloc.dart';
-import 'package:flood_mobile/Blocs/graph_bloc/graph_bloc.dart';
+import 'package:flood_mobile/Blocs/user_interface_bloc/user_interface_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class BlocProviders {
   static List<BlocProvider> multiBlocProviders = [
@@ -59,6 +60,9 @@ class BlocProviders {
     ),
     BlocProvider<UserInterfaceBloc>(
       create: (context) => UserInterfaceBloc(),
+    ),
+    BlocProvider<PowerManagementBloc>(
+      create: (context) => PowerManagementBloc(),
     ),
   ];
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,16 @@
 import 'package:awesome_notifications/awesome_notifications.dart';
+import 'package:flood_mobile/Blocs/bloc_provider_list.dart';
 import 'package:flood_mobile/Blocs/language_bloc/language_bloc.dart';
-import 'package:flood_mobile/Notifications/notification_controller.dart';
 import 'package:flood_mobile/Blocs/theme_bloc/theme_bloc.dart';
 import 'package:flood_mobile/Notifications/notification_channels.dart';
+import 'package:flood_mobile/Notifications/notification_controller.dart';
+import 'package:flood_mobile/Route/route_generator.dart';
+import 'package:flood_mobile/l10n/l10n.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
-import 'package:flood_mobile/Route/route_generator.dart';
-import 'package:flood_mobile/Blocs/bloc_provider_list.dart';
-import 'package:flood_mobile/l10n/l10n.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
