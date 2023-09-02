344342c66c0b5987a219e42f287c52c21297bb70
amit patel <amitamrutiya2210@gmail.com>
2023-09-02 13:28:44 +0530

    add changes in login screen url hint textfield

--- lib/Pages/home_screen/widgets/rss_feed_home_page.dart
+++ lib/Pages/home_screen/widgets/rss_feed_home_page.dart
@@ -1011,7 +1011,7 @@ class _RSSFeedHomePageState extends State<RSSFeedHomePage>
                                                           Padding(
                                                             padding:
                                                                 const EdgeInsets
-                                                                        .only(
+                                                                    .only(
                                                                     top: 20),
                                                             child: Column(
                                                               crossAxisAlignment:
@@ -1019,12 +1019,15 @@ class _RSSFeedHomePageState extends State<RSSFeedHomePage>
                                                                       .start,
                                                               children: [
                                                                 Padding(
-                                                                  padding: const EdgeInsets
+                                                                  padding:
+                                                                      const EdgeInsets
                                                                           .only(
-                                                                      right: 20,
-                                                                      left: 20,
-                                                                      bottom:
-                                                                          5),
+                                                                          right:
+                                                                              20,
+                                                                          left:
+                                                                              20,
+                                                                          bottom:
+                                                                              5),
                                                                   child: Text(
                                                                     l10n.selected_magnet_link,
                                                                     style: TextStyle(
@@ -1043,7 +1046,7 @@ class _RSSFeedHomePageState extends State<RSSFeedHomePage>
                                                                 ),
                                                                 Padding(
                                                                   padding: const EdgeInsets
-                                                                          .only(
+                                                                      .only(
                                                                       left:
                                                                           20.0,
                                                                       right:
@@ -1384,7 +1387,7 @@ class _RSSFeedHomePageState extends State<RSSFeedHomePage>
                                                           ? Padding(
                                                               padding:
                                                                   const EdgeInsets
-                                                                          .only(
+                                                                      .only(
                                                                       top: 8.0),
                                                               child: Container(
                                                                 height: 1,
